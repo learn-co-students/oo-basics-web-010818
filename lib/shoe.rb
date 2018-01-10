304e036a7b0f3ed15eb4a6587104d5454e1cd1b2
krdiamond <github email address>
2018-01-10 11:52:59 -0500

    solved lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand="", codition="old")
+    @brand = brand
+    @condition = condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
